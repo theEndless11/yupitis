bf4527ff07e6a9e68d5127d7d0c01d3501d9c16c
theEndless11 <aaa380144@gmail.com>
2025-03-17 16:50:20 +0530

    Update PostFinal.js

--- api/PostFinal.js
+++ api/PostFinal.js
@@ -59,8 +59,7 @@ module.exports = async function handler(req, res) {
                 likedBy: [],
                 dislikedBy: [],
                 comments: [],
-                photo: photoUrl,
-                profilePicture
+                photo: photoUrl
             };
 
             // Publish the new post to Ably
