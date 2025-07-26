615823aab5fefa3cd8ced8bd1a3b7fbf728bdac9
theEndless11 <aaa380144@gmail.com>
2025-07-26 11:05:23 +0530

    Create postgress.js

---
+++ utils/postgress.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'socketio',
+  host: 'mnz.domcloud.co',
+  database: 'socketio_msg',
+  password: 'zK2(j6)Mn6sOzL)87W',
+  port: 5432,
+  max: 100,
+  idleTimeoutMillis: 30000,
+  ssl: { rejectUnauthorized: false }, // for cloud hosting
+});
+
+pool.on('error', (err) => {
+  console.error('Postgres idle client error', err);
+});
+
+module.exports = pool;
+
