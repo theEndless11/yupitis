92e7086ff834d961dbea4fde813e8f813904e78b
theEndless11 <aaa380144@gmail.com>
2025-10-26 19:36:38 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -2,14 +2,14 @@ import mysql from 'mysql2';
 
 // Create a connection pool to your MySQL database
 const pool = mysql.createPool({
-  host: 'srv787.hstgr.io',   // e.g. localhost or a remote MySQL server
-  user: 'u208245805_Rokiscooking12',   // Your MySQL username
-  password: 'Rokiscooking12@',  // Your MySQL password
-  database: 'u208245805_Rokiscooking12',  // Your MySQL database name
+  host: 'srv1100.hstgr.io',   // e.g. localhost or a remote MySQL server
+  user: 'u289173202_Crypto21',   // Your MySQL username
+  password: 'Crypto21@@',  // Your MySQL password
+  database: 'u289173202_Crypto21',  // Your MySQL database name
   waitForConnections: true,      // Wait for available connection slots
-  connectionLimit: 10,           // Max number of connections in the pool
+  connectionLimit: 100,           // Max number of connections in the pool
   queueLimit: 0                  // Max number of requests to queue before returning an error
 });
-
 // Wrap the pool to return promises (using async/await)
 export const promisePool = pool.promise();
+
